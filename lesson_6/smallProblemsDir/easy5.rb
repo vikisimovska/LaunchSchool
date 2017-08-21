1fbd8b7b9061cb5abc22c5a935b338c3063773c2
Violeta Simovska <vikisimovska27@gmail.com>
2017-08-21 11:20:17 -0700

    up to num 5

--- lesson_6/smallProblemsDir/easy5.rb
+++ lesson_6/smallProblemsDir/easy5.rb
@@ -87,11 +87,16 @@ end
 # returns a hash that shows the number of words of different sizes.
 #Words consist of any string of characters that do not include a space.
 def word_sizes(str)
-  hash = str.split.group_by{ |word| word.size}
-  p hash.map{ |k,v| :k=>v.size}
+  hash = Hash.new(0)
+  str.split.each do |word|
+    hash[word.size] += 1
+  end
+  hash
 end
 
-p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+# p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
 # p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
 # p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
 # p word_sizes('') == {}
+
+#5.
