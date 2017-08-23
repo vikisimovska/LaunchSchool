e95819fdece37abf04f8ba25ef96f70ac1cc8411
Violeta Simovska <vikisimovska27@gmail.com>
2017-08-23 12:56:16 -0700

    up to #4

--- lesson_6/smallProblemsDir/easy6.rb
+++ lesson_6/smallProblemsDir/easy6.rb
@@ -2,17 +2,43 @@ system("clear")
 
 #2. Write a method that takes an array of strings, and returns an array of the
 #same string values, except with the vowels (a, e, i, o, u) removed.
-# def remove_vowels(arr)
-#   arr.delete('aeiouAEIOU')
-#   arr
-# end
+def remove_vowels(arr)
+  arr.map { |str| str.delete("aeiouAEIOU")}
+end
 def remove_vowels(strings)
   for string in strings
-    a = string.chars.delete('aeiouAEIOU')
-
+    string.delete!('aeiouAEIOU')
   end
-p a
 end
- remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) #== %w(bcdfghjklmnpqrstvwxyz)
-# p remove_vowels(%w(green YELLOW black white)) #== %w(grn YLLW blck wht)
+def remove_vowels(arr)
+  arr.each{ |str| str.delete!("aeiouAEIOU")}
+end
+# p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+# p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
 # p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
+
+#3.Write a method that calculates and returns the index of the first Fibonacci
+#number that has the number of digits specified as an argument.
+#(The first Fibonacci number has index 1.)
+# def find_fibonacci_index_by_length(num_digits)
+#   fib = [1, 1]
+#   while fib[-1].to_s.size != num_digits do
+#     fib << (fib[-1] + fib[-2])
+#   end
+#    fib.index(fib[-1])+1
+# end
+def find_fibonacci_index_by_length(num_digits)
+  fib = [1, 1]
+  until fib[-1].to_s.size == num_digits do #read: se dodeka...
+    fib << (fib[-1] + fib[-2])
+  end
+   fib.index(fib[-1])+1
+end
+
+# p find_fibonacci_index_by_length(2) == 7
+# p find_fibonacci_index_by_length(10) == 45
+# p find_fibonacci_index_by_length(100) == 476
+# p find_fibonacci_index_by_length(1000) == 4782
+# p find_fibonacci_index_by_length(10000) == 47847
+
+#4.
