06de6940ca8355aa8dea8651d368741e89f31a34
Violeta Simovska <vikisimovska27@gmail.com>
2017-08-23 13:39:01 -0700

    up to #5

--- lesson_6/smallProblemsDir/easy6.rb
+++ lesson_6/smallProblemsDir/easy6.rb
@@ -41,4 +41,43 @@ end
 # p find_fibonacci_index_by_length(1000) == 4782
 # p find_fibonacci_index_by_length(10000) == 47847
 
-#4.
+#4.Write a method that takes an Array as an argument, and reverses its elements
+#in place; that is, mutate the Array passed into this method. The return value
+#should be the same Array object.
+
+#You may not use Array#reverse or Array#reverse!.
+def reverse!(arr)
+  temp = arr.dup
+  arr.map!.with_index(1) { |_, i|  p temp[-i] }
+end
+def reverse!(arr)
+  arr.sort_by!.with_index { |_, idx| -idx }
+end
+def reverse!(arr)
+  dup = arr.dup
+  arr.each_with_index { |_, i| arr[i] = dup.pop }
+end
+def reverse!(arr)
+  arr.each_with_index { |_, idx|arr.insert(idx, arr.pop) }
+end
+#?????????????????????????
+def reverse!(arr)
+  arr.sort_by! { |i| arr.length - arr.index(i) }
+end
+
+# list = [1,2,3,4]
+# result = reverse!(list) # => [4,3,2,1]
+# p list == [4, 3, 2, 1]
+# p list.object_id == result.object_id
+#
+# list = %w(a b c d e)
+# reverse!(list) # => ["e", "d", "c", "b", "a"]
+# p list == ["e", "d", "c", "b", "a"]
+#
+# list = ['abc']
+# reverse!(list) # => ["abc"]
+# p list == ["abc"]
+#
+# list = []
+# reverse!(list) # => []
+# p list == []
