d3da2030b6575b8d8ce16ee0880597031b378842
Violeta Simovska <vikisimovska27@gmail.com>
2017-08-28 23:14:33 -0700

    finished ex.3

--- lesson_6/smallProblemsDir/medium1.rb
+++ lesson_6/smallProblemsDir/medium1.rb
@@ -47,4 +47,37 @@ end
 # p rotate_rightmost_digits(735291, 6) == 352917
 
 #CHECK THE REST OF THE SOLUTIONS!!!
-#3.
+#3.If you take a number like 735291, and rotate it to the left, you get 352917.
+# If you now keep the first digit fixed in place, and rotate the remaining digits,
+# you get 329175. Keep the first 2 digits fixed in place and rotate again to 321759.
+#Keep the first 3 digits fixed in place and rotate again to get 321597. Finally,
+#keep the first 4 digits fixed in place and rotate the final 2 digits to get
+#321579. The resulting number is called the maximum rotation of the original number.
+
+#Write a method that takes an integer as argument, and returns the maximum
+#rotation of that argument. You can (and probably should) use the
+#rotate_rightmost_digits method from the previous exercise.
+def max_rotation(num)
+  arr = num.to_s.split("")
+  first_rotation = arr[1..-1] + [arr[0]]
+  result = first_rotation
+  if arr.size == 1
+    return num
+  elsif arr.size == 2
+    return first_rotation.join.to_i
+  else
+    rotation_times = first_rotation.size - 2
+
+    1.upto(rotation_times) do |i|
+      rotating_digit = result.delete_at(i)
+      result.push(rotating_digit)
+    end
+  end
+   return result.join.to_i
+end
+
+p max_rotation(735291) == 321579
+p max_rotation(3) == 3
+p max_rotation(35) == 53
+p max_rotation(105) == 15 # the leading zero gets dropped
+p max_rotation(8_703_529_146) == 7_321_609_845
