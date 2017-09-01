325e42b746927bbae1c904ed598259d560b1777e
Violeta Simovska <vikisimovska27@gmail.com>
2017-08-31 23:20:33 -0700

    num4 exc.

--- lesson_6/smallProblemsDir/medium1.rb
+++ lesson_6/smallProblemsDir/medium1.rb
@@ -87,3 +87,21 @@ end
 # p max_rotation(35) == 53
 # p max_rotation(105) == 15 # the leading zero gets dropped
 # p max_rotation(8_703_529_146) == 7_321_609_845
+
+#4.You have a bank of switches before you numbered from 1 to 1000. Each switch is
+#connected to exactly one light that is initially off. You walk down the row of 
+#switches, and turn every one of them on. Then, you go back to the beginning and
+#toggle switches 2, 4, 6, and so on. Repeat this for switches 3, 6, 9, and so on,
+#and keep going until you have been through 1000 repetitions of this process.
+
+#Write a program that determines which lights are on at the end.
+
+#
+#round 1: every light is turned on
+#round 2: lights 2 and 4 are now off; 1, 3, 5 are on
+#round 3: lights 2, 3, and 4 are now off; 1 and 5 are on
+#round 4: lights 2 and 3 are now off; 1, 4, and 5 are on
+#round 5: lights 2, 3, and 5 are now off; 1 and 4 are on
+#The result is that 2 lights are left on, lights 1 and 4.
+
+#With 10 lights, 3 lights are left on: lights 1, 4, and 9.
