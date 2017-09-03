e98d7136a91588c85335d9111eea3b437d309afc
Violeta Simovska <vikisimovska27@gmail.com>
2017-09-02 23:31:07 -0700

    partial ex.4

--- lesson_6/smallProblemsDir/medium1.rb
+++ lesson_6/smallProblemsDir/medium1.rb
@@ -89,12 +89,19 @@ end
 # p max_rotation(8_703_529_146) == 7_321_609_845
 
 #4.You have a bank of switches before you numbered from 1 to 1000. Each switch is
-#connected to exactly one light that is initially off. You walk down the row of 
+#connected to exactly one light that is initially off. You walk down the row of
 #switches, and turn every one of them on. Then, you go back to the beginning and
 #toggle switches 2, 4, 6, and so on. Repeat this for switches 3, 6, 9, and so on,
 #and keep going until you have been through 1000 repetitions of this process.
 
 #Write a program that determines which lights are on at the end.
+hash = Hash.new
+1.times do |i|
+  hash["switch"] = "on"
+ end
+ 
+
+
 
 #
 #round 1: every light is turned on
