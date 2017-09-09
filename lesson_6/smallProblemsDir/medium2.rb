0fe77ba45281b10ea32d3e1624ee1a0a55a029af
Violeta Simovska <vikisimovska27@gmail.com>
2017-09-08 22:55:45 -0700

    begine ex.2

--- lesson_6/smallProblemsDir/medium2.rb
+++ lesson_6/smallProblemsDir/medium2.rb
@@ -1,22 +1,7 @@
 system("clear")
-#1.Longest Sentence
-
-
-str = "But, in a larger sense, we can not dedicate, we can not
-consecrate, we can not hallow this ground. The brave
-men, living and dead, who struggled here, have
-consecrated it, far above our poor power to add or
-detract. The world will little note, nor long remember
-what we say here, but it can never forget what they
-did here. It is for us the living, rather, to be dedicated
-here to the unfinished work which they who fought
-here have thus far so nobly advanced. It is rather for
-us to be here dedicated to the great task remaining
-before us -- that from these honored dead we take
-increased devotion to that cause for which they gave
-the last full measure of devotion -- that we here highly
-resolve that these dead shall not have died in vain
--- that this nation, under God, shall have a new birth
-of freedom -- and that government of the people, by
-the  people, for  the people, shall not perish from the
-earth. "
+#1.Longest Sentence & num of words
+text = File.read('/Users/violetasimovska/LaunchSchool/textFile')
+line = text.split(/\.|\?|!/)
+p largest_sentence = line.max_by{|sentence| sentence.size}.strip
+p largest_sentence.split.size
+p largest_sentence.size
