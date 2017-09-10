c8a141fb55bee04f0ecf631988dd35c8897d2cac
Violeta Simovska <vikisimovska27@gmail.com>
2017-09-09 23:17:55 -0700

    complited medium2.rb

--- lesson_6/smallProblemsDir/medium2.rb
+++ lesson_6/smallProblemsDir/medium2.rb
@@ -430,10 +430,43 @@ end
 #10.Write a method that computes the difference between the square of the sum of
 # the first n positive integers and the sum of the squares of the first n positive integers.
 def sum_square_difference(n)
-  p ((1..n).sum ** 2 )
+  square_sum = (1..n).sum ** 2
+  sum_of_squares = (1..n).map{|ele| ele**2}.sum
+  square_sum - sum_of_squares
 end
+#other SOLUTIONS
+def sum_square_difference(n)
+  sum = 0
+  sum_of_squares = 0
 
-p sum_square_difference(3) == 22 # -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
-p sum_square_difference(10) == 2640
-p sum_square_difference(1) == 0
-p sum_square_difference(100) == 25164150
+  1.upto(n) do |value|
+    sum += value
+    sum_of_squares += value**2
+  end
+
+  sum**2 - sum_of_squares
+end
+#******************************
+def sum_square_difference(n)
+  range = (1..n)
+  range.reduce(:+)**2 - range.reduce { |sum, num| sum + num**2 }
+end
+#******************************
+def sum_square_difference(n)
+  sum_square = (1..n).reduce(:+)**2
+  square_sum = (1..n).reduce { |total, num| total += num**2 }
+
+  sum_square - square_sum
+end
+#*******************************
+def sum_square_difference(int)
+  (1..int).sum ** 2 - (1..int).map { |i| i ** 2 }.sum
+end
+#******************************
+def sum_square_difference(n)
+  (1..n).reduce(:+)**2 - (1..n).map(&:abs2).reduce(:+)
+end
+# p sum_square_difference(3) == 22 # -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+# p sum_square_difference(10) == 2640
+# p sum_square_difference(1) == 0
+# p sum_square_difference(100) == 25164150
